e14c0dbaaa59f0f56db993a76a62634fc3e9536d
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-25 02:14:35 +0530

    Added : Hello World ReactAPP

---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Hello Suraj</h1>
+// }
+//ABOVE WE USE Normal Way , instead use arrow function: Render 1
+const Greet = () => <h1>Hello Suraj</h1>
+export default Greet
